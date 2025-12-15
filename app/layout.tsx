df495550dadb40dd0fa35212758f276bb4c19139
staajsolutions <staajsupport@staaj.com>
2025-12-15 15:26:34 -0700

    Update layout.tsx
    missing code for Hubspot fix!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
